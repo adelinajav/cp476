ea58e6d27d578affbf140c9b9c4d3da69f897c0e
Adelina Javgureanu <126191298+adelinajav@users.noreply.github.com>
2025-03-21 17:10:44 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -15,12 +15,8 @@ $studentID = $_SESSION["studentID"];
 
 //get the specific studentID courses and grades
 $sql = "SELECT CourseCode, Test1, Test2, Test3, FinalExam FROM CourseTable WHERE StudentID = ?";
-
-//1. preparing
 $stmt = $conn->prepare($sql);
-//2. binding
 $stmt->bind_param("i", $studentID);
-//3. executing
 $stmt->execute();
 
 $result = $stmt->get_result(); 
@@ -28,29 +24,38 @@ $result = $stmt->get_result();
 //ui section
 echo "<h2>Student Dashboard</h2>";
 echo "<p>Welcome, Student ID: $studentID</p>";
-echo "<table border='1'>
+echo "<table border='2'>
         <tr>
-            <th>Course Code</th>
-            <th>Test 1</th>
-            <th>Test 2</th>
-            <th>Test 3</th>
-            <th>Final Exam</th>
+		<th>Course Code</th>
+		<th>Test 1</th>
+		<th>Test 2</th>
+		<th>Test 3</th>
+		<th>Final Exam</th>
+		<th>Final Grades</th>
+		<th>Actions</th>
         </tr>";
 
 //while loop because there can be multiple courses
-while ($row = $result->fetch_assoc()) {
-    echo "<tr>
-            <td>{$row['CourseCode']}</td>
-            <td>{$row['Test1']}</td>
-            <td>{$row['Test2']}</td>
-            <td>{$row['Test3']}</td>
-            <td>{$row['FinalExam']}</td>
-        </tr>";
+while($row = $result->fetch_assoc()){
+	$finalGrade = ($row['Test1']*0.2)+($row['Test2']*0.2)+($row['Test3']*0.2)+($row['FinalExam']*0.4);
+	$finalGrade = number_format($finalGrade, 1);
+	
+	echo "<tr>
+		<td>{$row['CourseCode']}</td>
+		<td>{$row['Test1']}</td>
+		<td>{$row['Test2']}</td>
+		<td>{$row['Test3']}</td>
+		<td>{$row['FinalExam']}</td>
+		<td>%$finalGrade</td>
+		<td>
+			<a href='update.php?studentID={$studentID}&courseCode={$row['CourseCode']}'>Update</a>|
+			<a href='delete.php?studentID={$studentID}&courseCode={$row['CourseCode']}' onclick='return confirm(\"Are you sure you want to delete this course?\");'>Delete</a>
+
+		</td>
+	</tr>";
 }
 
 echo "</table>";
-//logout button to go back to login page
 ?>
 <br>
 <a href="logout.php">Logout</a>
-
